2d3d032175925a753bc491760c9241ed2cc143c6
yarlie1 <yarlie@gmail.com>
2026-04-04 11:50:11 -0700

    app/login/page.tsx 만들기

---
+++ app/login/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useState } from "react";
+import { createClient } from "../../lib/supabase/client";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const supabase = createClient();
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleLogin = async () => {
+    setMessage("");
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      setMessage(error.message);
+    } else {
+      router.push("/dashboard");
+    }
+  };
+
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Log In</h1>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+
+      <button onClick={handleLogin}>Log In</button>
+
+      {message && <p>{message}</p>}
+
+      <div style={{ marginTop: 20 }}>
+        <a href="/">
+          <button>Back to Home</button>
+        </a>
+      </div>
+    </main>
+  );
+}
