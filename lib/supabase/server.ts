6b9e3beb96016d3243fa9cde3050649bf5fb8165
yarlie1 <yarlie@gmail.com>
2026-04-03 18:53:25 -0700

    Create server.ts

---
+++ lib/supabase/server.ts
@@ -0,0 +1,20 @@
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+
+export function createClient() {
+  const cookieStore = cookies();
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set() {},
+        remove() {},
+      },
+    }
+  );
+}
