6211fcbf5238b91eb0464ea23d47118f120c071a
yarlie1 <yarlie@gmail.com>
2026-04-06 13:32:15 -0700

    layout.tsx 업데이트

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 
 import "./globals.css";
 import { useEffect, useState } from "react";
+import Script from "next/script";
 import { createClient } from "../lib/supabase/client";
 import TopNav from "./components/TopNav";
 
@@ -38,6 +39,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-[#f7f1ea] text-[#2f2a26]">
+        <Script
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+          strategy="beforeInteractive"
+        />
         <TopNav userEmail={userEmail} onLogout={handleLogout} />
         {children}
       </body>
