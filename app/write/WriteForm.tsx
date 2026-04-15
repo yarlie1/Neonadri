167c2ec94d5fb2a46ca33a2cc3d06734543d4ef1
yarlie1 <yarlie1@users.noreply.github.com>
2026-04-15 08:48:13 -0700

    Fix map picker icon import

--- app/write/WriteForm.tsx
+++ app/write/WriteForm.tsx
@@ -17,6 +17,7 @@ import {
   BookOpen,
   Laptop,
   Camera,
+  Map,
   MapPin,
   Clock,
   User,
--- app/write/[id]/EditMeetupForm.tsx
+++ app/write/[id]/EditMeetupForm.tsx
@@ -18,6 +18,7 @@ import {
   BookOpen,
   Laptop,
   Camera,
+  Map,
   MapPin,
   Clock,
   User,
